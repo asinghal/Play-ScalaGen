3725995acf956d6d68aa8d717c3709cc8cd0ab66
Aishwarya Singhal <asinghal79@gmail.com>
2011-08-15 12:30:02 +0530

    added JQGrid support

--- src/play/modules/scalagen/jpa/UtilsGenerator.java
+++ src/play/modules/scalagen/jpa/UtilsGenerator.java
@@ -15,15 +15,32 @@ public class UtilsGenerator {
 	 * Generates utility classes needed for the code to function.
 	 */
 	public static void generate() {
-		String template = TemplatesHelper.getTemplate("utils/HttpBinder");
 
+		// generate the form binder
 		if (!TemplatesHelper.exists("app", "utils", "HttpBinder.scala")) {
+			String template = TemplatesHelper.getTemplate("utils/HttpBinder");
 			TemplatesHelper.flush("app", "utils", "HttpBinder.scala", template);
-			
+
 			template = TemplatesHelper.getTemplate("utils/converterTests");
-			TemplatesHelper.flush("test", "tests", "ConverterTests.scala", template);
+			TemplatesHelper.flush("test", "tests", "ConverterTests.scala",
+					template);
 		} else {
-			System.out.println("* app/utils/HttpBinder.scala already exists. Skipping.");
+			System.out
+					.println("* app/utils/HttpBinder.scala already exists. Skipping.");
+		}
+
+		// generate the JQ Grid helper
+		if (!TemplatesHelper.exists("app", "utils", "JQGridHelper.scala")) {
+			String template = TemplatesHelper.getTemplate("utils/JQGridHelper");
+			TemplatesHelper.flush("app", "utils", "JQGridHelper.scala",
+					template);
+
+			template = TemplatesHelper.getTemplate("utils/jqGridHelperTests");
+			TemplatesHelper.flush("test", "tests", "JQGridHelperTests.scala",
+					template);
+		} else {
+			System.out
+					.println("* app/utils/JQGridHelper.scala already exists. Skipping.");
 		}
 	}
 }
